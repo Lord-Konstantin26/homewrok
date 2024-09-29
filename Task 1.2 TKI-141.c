99e8a47dd4f71e97aaa39de6409f1d406f97abfe
KonstantinKomarov <konstakom2605@mail.ru>
2024-09-29 22:57:58 +0300

    Add files via upload

---
+++ Task 1.2 TKI-141.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+double Input(void);
+
+double GetInch(const double cantimetre);
+
+int main()
+{
+	puts("Введите количество сантиметров");
+	double cantimetre = Input();
+	printf_s("Количество дюймов %lf", GetInch(cantimetre));
+
+	return 0;
+}
+
+double Input(void)
+{
+	double value = 0.0;
+	scanf_s("%lf", &value);
+	return value;
+}
+
+double GetInch(const double cantimetre)
+{
+	return cantimetre / 2.5;
+}
