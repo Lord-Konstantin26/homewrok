bc1549a470d9172e1863be05bb251ee568d80abc
KonstantinKomarov <konstakom2605@mail.ru>
2024-09-30 09:35:16 +0300

    Add files via upload
    Загрузил решение задания 1.2

---
+++ Task 1.2 TKI-141.c
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+
+/*
+* @brief Считывает вещественное число.
+* @return Вещественное число.
+*/ 
+double Input(void);
+
+/*
+* @brief Рассчитывет количестуво дюймов.
+* @param cantimetre Количество сантиметров.
+* @return Количество дюймов
+*/
+double GetInch(const double cantimetre);
+
+/*
+* @brief Точка входа в программу.
+* @retyrn \c 0 в случае выполнения без ошибок.
+*/
+int main()
+{
+	puts("Введите количество сантиметров");
+	double cantimetre = Input();
+	printf_s("Количество дюймов %lf", GetInch(cantimetre));
+
+	return 0;
+}
+
+double Input(void)
+{
+	double value = 0.0;
+	scanf_s("%lf", &value);
+	return value;
+}
+
+double GetInch(const double cantimetre)
+{
+	return cantimetre / 2.5;
+}
